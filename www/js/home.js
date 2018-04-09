4e92bc8064803f57490d83648231b5933b179c9b
EzequielM <eze.monsal@gmail.com>
2018-04-09 01:59:25 -0300

    cambiar perfil

--- www/js/home.js
+++ www/js/home.js
@@ -19,7 +19,7 @@ function onBackKeyDown() {
             'Confirmar',            // title
             ['Si', 'No']          // buttonLabels
         );
-    }if(localStorage.localization == 2){
+    } if (localStorage.localization == 2) {
         window.location.href = 'home.html';
     } else {
         $('#options').addClass("d-none");
@@ -68,49 +68,49 @@ function buildNewDoc() {
 }
 
 function buildMaterial() {
-$.ajax({
-    type: "GET", //GET, POST, PUT
-    url: 'http://69.164.202.85:8000/api/getMaterialProfesor',  //the url to call
-    //Data sent to server
-    beforeSend: function (xhr) {   //Include the bearer token in header
-        xhr.setRequestHeader("Authorization", localStorage.token);
-    }
-}).done(function (response) {
-console.log(response);
-    var listamaterial = '';
-    $.each(response, function (key, element) {
-        var fecha = element.created_at.split(" ");
-        listamaterial += '<div class="card mb-2" onclick=""><div class="row m-0 p-2"><div class="col-4 p-0 text-center">'+
-                         '<img src="http://69.164.202.85/app/resources/img/prueba.jpg" style="height:100%; width:100%"></div>'+
-                         '<div class="col-7 p-0 ml-auto pl-1" style="background-color:white;position:relative;">'+
-                         '<p class="card-title">Asignatura: <span class="card-text">' + element.asignatura.nombre_asignatura + '</span></p>'+
-                         '<p class="card-title">Título: <span class="card-text">' + element.titulo_material + '</span></p>'+
-                         '<p class="card-title">Fecha: <span class="card-text">' + fecha[0] + '</span></p>'+
-                         '<div id="likematerial' + element.id +'" class="like mt-auto ml-auto" onclick="likematerial('+element.id+')" style="bottom:0px; position:absolute; right:0.3em">'+
-                         '<i  class="far fa-heart"></i></div></div></div></div>';
-    });
+    $.ajax({
+        type: "GET", //GET, POST, PUT
+        url: 'http://69.164.202.85:8000/api/getMaterialProfesor',  //the url to call
+        //Data sent to server
+        beforeSend: function (xhr) {   //Include the bearer token in header
+            xhr.setRequestHeader("Authorization", localStorage.token);
+        }
+    }).done(function (response) {
+        console.log(response);
+        var listamaterial = '';
+        $.each(response, function (key, element) {
+            var fecha = element.created_at.split(" ");
+            listamaterial += '<div class="card mb-2" onclick=""><div class="row m-0 p-2"><div class="col-4 p-0 text-center">' +
+                '<img src="http://69.164.202.85/app/resources/img/prueba.jpg" style="height:100%; width:100%"></div>' +
+                '<div class="col-7 p-0 ml-auto pl-1" style="background-color:white;position:relative;">' +
+                '<p class="card-title">Asignatura: <span class="card-text">' + element.asignatura.nombre_asignatura + '</span></p>' +
+                '<p class="card-title">Título: <span class="card-text">' + element.titulo_material + '</span></p>' +
+                '<p class="card-title">Fecha: <span class="card-text">' + fecha[0] + '</span></p>' +
+                '<div id="likematerial' + element.id + '" class="like mt-auto ml-auto" onclick="likematerial(' + element.id + ')" style="bottom:0px; position:absolute; right:0.3em">' +
+                '<i  class="far fa-heart"></i></div></div></div></div>';
+        });
 
         $("#options").load("material.html", function () {
             $("#listamaterial").html(listamaterial);
         });
 
-       $('#options').removeClass("d-none");
-       $('#modulos').addClass("d-none");
-       localStorage.localization = 1;
-}).fail(function (err)  {
-    alert("chao");
-});
+        $('#options').removeClass("d-none");
+        $('#modulos').addClass("d-none");
+        localStorage.localization = 1;
+    }).fail(function (err) {
+        alert("chao");
+    });
 }
-function likematerial(idmaterial){
-        console.log("hola");
-        if($("#likematerial" + idmaterial).hasClass('like')){
-            $("#likematerial" + idmaterial).html('<i  class="fas fa-heart"></i>');
-            $("#likematerial" + idmaterial).removeClass('like');
-        }else{
-            $("#likematerial" + idmaterial).html('<i  class="far fa-heart"></i>');
-            $("#likematerial" + idmaterial).addClass('like');
-        }
+function likematerial(idmaterial) {
+    console.log("hola");
+    if ($("#likematerial" + idmaterial).hasClass('like')) {
+        $("#likematerial" + idmaterial).html('<i  class="fas fa-heart"></i>');
+        $("#likematerial" + idmaterial).removeClass('like');
+    } else {
+        $("#likematerial" + idmaterial).html('<i  class="far fa-heart"></i>');
+        $("#likematerial" + idmaterial).addClass('like');
     }
+}
 function buildFavor() {
     $("#options").load("documentos.html");
     document.getElementById('options').style.display = "block";
@@ -134,24 +134,42 @@ function verPerfil() {
             xhr.setRequestHeader("Authorization", localStorage.token);
         }
     }).done(function (response) {
-            console.log(response);
-            $("#openperfil").html("Ver Modulos");
-            $("#openperfil").attr("onclick","salirtoModulos()");
-            $("#userimage").attr("src", response.profesor.url_foto_profesor);
-            $("#username").html(response.profesor.nombres_profesor + " " + response.profesor.apellidos_profesor);
-            $("#userprofesion").html(response.profesor.profesion);
-            $("#usercargo").html(response.profesor.cargo);
-            $("#useremail").html(response.email);
-           $('#options').addClass("d-none");
-           $('#modulos').addClass("d-none");
-           $('#perfilUser').removeClass("d-none");
-           localStorage.localization = 1;
-    }).fail(function (err)  {
+        console.log(response);
+        document.getElementById('menupartida').innerHTML = '<li id="openperfil" class="p-2" onclick="salirtoModulos()">Ver Modulos</li>' +
+            '<li class="p-2"><button class="btn btn-modal txt-azul pl-0" style="background-color: white" data-toggle="modal" data-target="#fsModal">Editar Perfil </button></li>' +
+            '<li class="p-2"><button class="btn btn-modal txt-azul pl-0" style="background-color: white" data-toggle="modal" data-target="#passModal">Cambiar Contraseña </button></li>' +
+            '<li class="p-2 txt-azul" onclick="logout()">Cerrar Sesión</li>';
+        $("#userimage").attr("src", response.profesor.url_foto_profesor);
+        $("#imageuserEdit").attr("src", response.profesor.url_foto_profesor);
+        $("#username").html(response.profesor.nombres_profesor + " " + response.profesor.apellidos_profesor);
+        $("#nombreUserEdit").val(response.profesor.nombres_profesor);
+        $("#apellidoUserEdit").val(response.profesor.apellidos_profesor);
+        $("#useremail").html(response.email);
+        $("#emailUserEdit").val(response.email);
+        $("#userprofesion").html(response.profesor.profesion);
+        $("#profesionUserEdit").val(response.profesor.profesion);
+        $("#usercargo").html(response.profesor.cargo);
+
+        $('#options').addClass("d-none");
+        $('#modulos').addClass("d-none");
+        $('#perfilUser').removeClass("d-none");
+        localStorage.localization = 1;
+    }).fail(function (err) {
         alert("chao");
     });
 }
-
+function changeimg() {
+    navigator.camera.getPicture(uploadPhoto, function (message) {
+        alert('get picture failed');
+    }, {
+            quality: 100,
+            destinationType: navigator.camera.DestinationType.FILE_URI,
+            sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+        });
+}
 function salirtoModulos() {
+    document.getElementById('menupartida').innerHTML = '<li id="openperfil" class="p-2" onclick="verPerfil()">Perfil de Usuario</li>' +
+        '<li class="p-2" onclick="logout()">Cerrar Sesión</li>';
     $('#options').addClass("d-none");
     $('#modulos').removeClass("d-none");
     $('#perfilUser').addClass("d-none");
